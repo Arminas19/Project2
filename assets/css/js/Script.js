f20c2c77408c4a43b38cbe3f0a88cf7647e8275d
Arminas19 <armeska14@gmail.com>
2021-10-01 22:19:16 +0000

    add alert pop up when you load

--- assets/css/js/Script.js
+++ assets/css/js/Script.js
@@ -1,1 +1,2 @@
-console.log("Hello world!");
+/* Alert pop-up when ever the user loads or reloads the page.*/
+alert(" ⓘ How To Play  ★ Match 3 bell Icons to get a win, Once the game is complete press the Reset button to start again. If you guess an incorrect icon you loss.");
