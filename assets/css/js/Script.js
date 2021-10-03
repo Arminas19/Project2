0938960b627d919e61aa6a30c319c94df9c50f23
Arminas19 <armeska14@gmail.com>
2021-10-03 13:47:34 +0000

    javaScript functions

--- assets/css/js/Script.js
+++ assets/css/js/Script.js
@@ -1,29 +1,47 @@
 /* Alert pop-up when ever the user loads or reloads the page.*/
-alert(" ⓘ How To Play  ★ Match 3 bell Icons to get a win, Once the game is complete press the Reset button to start again. If you guess an incorrect icon you loss.");
+//alert(" ⓘ How To Play  ★ Match 3 bell Icons to get a win, Once the game is complete press the Reset button to start again. If you guess an incorrect icon you loss.");
 
 document.addEventListener("DOMContentLoaded", function(){
-    let divs = document.getElementsByTagName("div");
+    let divs = document.getElementsByClassName("div-children");
     for(div of divs){
 
-        button.addEventListener("click", function(){
+        div.addEventListener("click", function(){
             if(this.getAttribute("data-type") === "reset"){
-                    resetGame();
+                alert("YOO");
+                    resetButton();
             }
             else{
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType);
+                 this.getAttribute("data-type");
+                runGame();
             }
 
         })
     }
+    
 });
 
 function runGame(){
-document.getElementsByClassName('div-children');
+    setImages();
 
 }
 
+function setImages(){
+ 
+    if (this.style.backgroundColor === "green") {
+        // this.style.backgroundColor = "green";
+     } else {
+         this.style.backgroundColor = "green";
+     }
+     
+ 
+ 
+ let divs = document.getElementsByClassName('div-children');
+ 
+ for (let i = 0; i < divs.length; i++) {
+     divs[i].addEventListener('click', boxClicked);
+ }
+}
 
-function resetGame(){
-
+function resetButton(){
+alert('You clicked the reset button');
 }
