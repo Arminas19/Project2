b50343d903ddf762d5eb345173c4c5020fa8386f
Arminas19 <armeska14@gmail.com>
2021-10-03 15:31:23 +0000

    worked with functions

--- assets/css/js/Script.js
+++ assets/css/js/Script.js
@@ -1,32 +1,36 @@
 /* Alert pop-up when ever the user loads or reloads the page.*/
-//alert(" ⓘ How To Play  ★ Match 3 bell Icons to get a win, Once the game is complete press the Reset button to start again. If you guess an incorrect icon you loss.");
+alert(" ⓘ How To Play  ★ Match 3 bell Icons to get a win, Once the game is complete press the Reset button to start again. If you guess an incorrect icon you loss.");
 
 document.addEventListener("DOMContentLoaded", function(){
     let divs = document.getElementsByClassName("div-children");
+    
     for(div of divs){
-
+        
         div.addEventListener("click", function(){
             if(this.getAttribute("data-type") === "reset"){
-                alert("YOO");
-                    resetButton();
+                resetButton();
             }
             else{
-                 this.getAttribute("data-type");
+                this.getAttribute("data-type")
+               
                 runGame();
             }
 
         })
     }
+    document.getElementById("reset-button").onclick = () => {
+        resetButton();
+        }
     
 });
 
 function runGame(){
     setImages();
-
 }
 
 function setImages(){
- 
+    let divs = document.getElementsByClassName('div-children');
+
     if (this.style.backgroundColor === "green") {
         // this.style.backgroundColor = "green";
      } else {
@@ -38,7 +42,7 @@ function setImages(){
  let divs = document.getElementsByClassName('div-children');
  
  for (let i = 0; i < divs.length; i++) {
-     divs[i].addEventListener('click', boxClicked);
+     divs[i].addEventListener('click', div);
  }
 }
 
