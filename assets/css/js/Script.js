88bb7c7dbb2e9b1ac71e5bd07b9edf42a9b5c328
Arminas19 <armeska14@gmail.com>
2021-10-04 13:13:56 +0000

    added randomize feature

--- assets/css/js/Script.js
+++ assets/css/js/Script.js
@@ -3,24 +3,52 @@ alert(" ⓘ How To Play  ★ Match 3 bell Icons to get a win, Once the game is c
 
 document.addEventListener("DOMContentLoaded", function(){
     
-let divs = document.getElementsByClassName('div-children');
-if(divs.onclick === "div-children"){
-runGame();
-}
-else{
-    let resetButton = document.getElementById('reset-button');
-    resetButton.onclick = function(){
-        resetButton();
-    }
-}
+ let divs = document.getElementsByClassName("div-children");
+for(div of divs){
 
+    div.addEventListener("click", function(){
+        if(this.getAttribute("data-type") === "submit"){
+                resetButton();
+        }
+        else{
+            this.getAttribute("data-type") === "div1-9";
+            runGame(getRandomImage);
+        }
+
+    })
+}
 });
 
-function runGame(){
 
+
+
+
+function runGame(getRandomImage){
+//alert('runGame function has been activated');
+
+let image_Array = [
+'bell (1).png',
+'cherry (1).png',
+'lemon (1).png',
+'orange (1).png',
+'watermelon (1).png'
+]
+
+function getRandomImage(){
+
+random_index = Math.floor(Math.random() * image_Array.length);
+
+selectedImage = image_Array[random_index]
+
+document.getElementsByClassName('image-div').src = `./images/${selectedImage}`
+
+return getRandomImage
+}
 
 }
 
 function resetButton(){
-   
+    alert('resetButton function has been activated');
+    getRandomImage();
+
 }
