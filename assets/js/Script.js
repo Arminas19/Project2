229a979a3ed636bd2a88d22e7b40e6da56764e7a
Arminas19 <armeska14@gmail.com>
2021-10-11 15:00:49 +0000

    replacing somold html

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -2,21 +2,20 @@ alert(" ⓘ How To Play  ★ Match 3 bell Icons to get a win, Once the game is c
 /*  Once the browser is fully loaded this functtion is called: The purpose of this function is to capture where the user clicked,
 their are currently 2 options. One for the reset button and the other one is for the getRandomImage function. 
 */
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
 
-    let divs = document.getElementsByClassName("div-children");
+    let divs = document.getElementsByClassName("Game-container");
     for (div of divs) {
 
-        div.addEventListener("click", function (event) {
-            
+        div.addEventListener("click", function(event) {
+
             getRandomImage(this);
             calculateAnswer(event.target);
         });
     }
     let resetButton = document.getElementById("reset-button");
     resetButton.addEventListener("click", resetButton);
-}
-);
+});
 
 /* This Array lays out all the images that will be needed for the getRandomImage(imageDiv) function.
 This function is used to get and set a random image out of the image arrry and display the image in a <img> tag.
@@ -34,7 +33,7 @@ let image_Array = [
 ];
 /* This functions puropse is to set a random image inside the <img> tag */
 function getRandomImage(imageDiv) {
-   
+
     // alert('getRandomImage function has been activated');
 
     random_index = Math.floor(Math.random() * image_Array.length);
@@ -64,29 +63,30 @@ if failedAttempt = 3 than the playerHasLost function is called.
 //let bell = 0;
 let bell = 0;
 let failedAttempt = 0;
+
 function calculateAnswer(element) {
     console.log('You have made it to calculateAnswer');
-  
-    
+
+
     //let img = document.getElementsByTagName('img')[this].src;
 
     console.log(element);
     console.log(window.getComputedStyle(element, null).backgroundImage);
 
     console.log('This is the element ' + element.style.backgroundImage);
-   // let imgsrc = document.getElementsByTagName("img").src;
+    // let imgsrc = document.getElementsByTagName("img").src;
 
     let switchUrl = 'url("assets/images/bell (1).png")';
-    
+
     switch (element.style.backgroundImage) {
         case switchUrl:
             bell += 1;
             alert(`You Found ${bell} bell icons`);
-            
+
             break;
-        case  !switchUrl:
-           // failedAttempt += 1;
-           // alert(`You have ${failedAttempt}/3 trys left`);
+        case !switchUrl:
+            // failedAttempt += 1;
+            // alert(`You have ${failedAttempt}/3 trys left`);
             break;
         default:
             failedAttempt += 1;
@@ -96,11 +96,11 @@ function calculateAnswer(element) {
     }
     console.log('This is the bell ' + bell);
     console.log('This is the failed Attemts ' + failedAttempt);
-   
-    if(bell === 3){
+
+    if (bell === 3) {
         playerHasWon();
     }
-    if(failedAttempt === 3){
+    if (failedAttempt === 3) {
         playerHasLost();
     }
     /* if(img === `url('assets/images/bell (1).png')`){
@@ -130,79 +130,84 @@ function calculateAnswer(element) {
 from being played by setting the images.style.backgroundImage to undifined and leaving the calculateAnswer function empty. */
 function playerHasWon() {
     alert('You Won!!');
-    
+
     let win = parseInt(document.getElementById('win').innerText);
-    document.getElementById('win').innerText = ++win; 
-    
+    document.getElementById('win').innerText = ++win;
+
     getRandomImage = (imageDiv) => {
-    let imagesArray = imageDiv.getElementsByClassName('image-div')[0];
-    imagesArray.style.backgroundImage = undefined;
+        let imagesArray = imageDiv.getElementsByClassName('image-div')[0];
+        imagesArray.style.backgroundImage = undefined;
     };
-    
+
     calculateAnswer = () => {
-    
+
     };
-    
-    }
-    
-    /* This function is the same like the playerHasWon function but inseade of incresing the won span tag 
-    this is incressing the loss span tag. */
-    function playerHasLost() {
+
+}
+
+/* This function is the same like the playerHasWon function but inseade of incresing the won span tag 
+this is incressing the loss span tag. */
+function playerHasLost() {
     alert(' You Lost :( ');
-    
+
     let loss = parseInt(document.getElementById('loss').innerText);
-    document.getElementById('loss').innerText = ++loss; 
-    
+    document.getElementById('loss').innerText = ++loss;
+
     getRandomImage = (imageDiv) => {
-    let imagesArray = imageDiv.getElementsByClassName('image-div')[0];
-    imagesArray.style.backgroundImage = undefined;
-       };
-    calculateAnswer = () => {
-    
+        let imagesArray = imageDiv.getElementsByClassName('image-div')[0];
+        imagesArray.style.backgroundImage = undefined;
     };
-    }
-    
-    
-     /* I use the resetButton and the resetGame functions too reset the images and to start the game over but in the same time
-     leave the scores just like they where before. */
-    function resetButton() {
-        alert('resetButton function has been activated');
-        resetGame();
-       
-    }
-    function resetGame(){
-        getRandomImage = (imageDiv) => {
-            
-    
-            random_index = Math.floor(Math.random() * image_Array.length);
-    
-            selectedImage = image_Array[random_index];
-        
-            
-            let imagesArray = imageDiv.getElementsByClassName('image-div')[0];
-            imagesArray.style.backgroundImage = `url('${selectedImage}')`;
-            };
-    
-            let bell = 0;
-            let failedAttempt = 0;
-    
-            calculateAnswer = (element) => {
-                console.log(element);
+    calculateAnswer = () => {
+
+    };
+}
+
+
+/* I use the resetButton and the resetGame functions too reset the images and to start the game over but in the same time
+leave the scores just like they where before. */
+function resetButton() {
+    alert('resetButton function has been activated');
+    resetGame();
+
+}
+
+function resetGame() {
+    getRandomImage = (imageDiv) => {
+
+        let newimagesArray = imageDiv.getElementsByClassName('image-div')[0];
+        newimagesArray.style = null;
+
+
+
+        random_index = Math.floor(Math.random() * image_Array.length);
+
+        selectedImage = image_Array[random_index];
+
+
+        let imagesArray = imageDiv.getElementsByClassName('image-div')[0];
+        imagesArray.style.backgroundImage = `url('${selectedImage}')`;
+    };
+
+    bell = 0;
+    failedAttempt = 0;
+
+    calculateAnswer = (element) => {
+        console.log(element);
         console.log(window.getComputedStyle(element, null).backgroundImage);
-    
+
         console.log('This is the element ' + element.style.backgroundImage);
-      
-    
+
+
         let switchUrl = 'url("assets/images/bell (1).png")';
-        
+
         switch (element.style.backgroundImage) {
             case switchUrl:
                 bell += 1;
                 alert(`You Found ${bell} bell icons`);
-                
+
                 break;
-            case  !switchUrl:
-              
+            case !switchUrl:
+
                 break;
             default:
                 failedAttempt += 1;
@@ -212,12 +217,12 @@ function playerHasWon() {
         }
         console.log('This is the bell ' + bell);
         console.log('This is the failed Attemts ' + failedAttempt);
-       
-                if(bell === 3){
-                    playerHasWon();
-                }
-                if(failedAttempt === 3){
-                    playerHasLost();
-                }
-            };
-    }
+
+        if (bell === 3) {
+            playerHasWon();
+        }
+        if (failedAttempt === 3) {
+            playerHasLost();
+        }
+    };
+}
